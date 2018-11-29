16fba3a3a80c489242549af42b24864fcbfe32f6
Noah Flint <noah.flint24@gmail.com>
2018-11-28 20:02:29 -0700

    added empty_turn_array test and turn method

--- lib/round.rb
+++ lib/round.rb
@@ -3,13 +3,13 @@ class Round
   attr_reader :deck
 
 
-def initialize(deck_argument)
+  def initialize(deck_argument)
   @deck = deck_argument
-end
+  end
 
-
-
-
+  def turns
+  turns = []
+  end
 
 
 end
