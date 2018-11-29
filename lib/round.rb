5d6f2ef77d98f0c013c76bbf3f559243adbaf903
Noah Flint <noah.flint24@gmail.com>
2018-11-29 13:20:57 -0700

    added current_card test and method

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,14 @@
 class Round
 
-  attr_reader :deck
-
+  attr_reader :deck,
+              :current_card
 
   def initialize(deck_argument)
-  @deck = deck_argument
+    @deck = deck_argument
+    @current_card = @deck.cards[0]
   end
 
   def turns
-  turns = []
+    @turns = []
   end
-
-
 end
