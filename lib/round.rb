1763a2d91a43c4b645a56a2eec7b6df81369df53
Noah Flint <noah.flint24@gmail.com>
2018-11-28 18:02:45 -0700

    added check_for_deck test, and deck attr_reader

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,10 @@
 class Round
 
-  
+  attr_reader :deck
+
 
 def initialize(deck_argument)
-  @round = deck_argument
+  @deck = deck_argument
 end
 
 
--- test/round_test.rb
+++ test/round_test.rb
@@ -19,6 +19,19 @@ class RoundTest < Minitest::Test
 
   end
 
+  def test_check_for_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_instance_of Deck, deck
+
+
+  end
+
 
 
 
